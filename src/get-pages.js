4be0a63587202e2ef7768ac0d8d194de627631ff
Dmitry Yevlakhov <tragenstolz@gmail.com>
2019-08-06 12:56:19 +0300

    init

---
+++ src/get-pages.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * Implement getPages function:
+ *
+ * Given an object, create script which returns array with pages.
+ *
+ * Examples:
+ * getPages({ totalCount: 42, perPage: 10 }) should return array with 5 numbers
+ *
+ * @param {object} phrase
+ *
+ * @return {array}
+ */
+const getPages = ({ totalCount, perPage }) => {
+  // write code here
+};
+
+module.exports = getPages;
